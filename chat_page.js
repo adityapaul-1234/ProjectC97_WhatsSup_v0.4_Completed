ed1f7321e9d1cc504d7d8177bd783f9bd20e1c88
Aditya Paul <76274584+adityapaul-1234@users.noreply.github.com>
2022-05-26 15:29:26 +0530

    Update chat_page.js

--- chat_page.js
+++ chat_page.js
@@ -37,7 +37,7 @@ function send(){
 function logout(){
     localStorage.removeItem("Room_Name");
     localStorage.removeItem("user_name");
-    window.location = "login_page.html";
+    window.location = "index.html";
 }
 
 function back(){
